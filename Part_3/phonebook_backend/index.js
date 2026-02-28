915c414d34ac6a6e61b9c0f9312e59975733594b
CHIPPADA HARISH <91726911+StarChip7@users.noreply.github.com>
2026-02-28 17:00:29 +0530

    modified folder name added underscore
    modified folder name added underscore

--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -76,7 +76,7 @@ app.post('/api/persons', (req, res) => {
     res.json(person)
 })
 
-PORT = 3001
+PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
 })
