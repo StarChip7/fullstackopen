d359213e988e42e35f083af524f2076a56d4b26a
CHIPPADA HARISH <91726911+StarChip7@users.noreply.github.com>
2026-03-01 11:25:05 +0530

    Part 3 excercises till 3.22
    Part 3 excercises till 3.22

---
+++ Part_3/phonebook/src/App.jsx
@@ -0,0 +1,153 @@
+import { useEffect, useState } from 'react'
+import axios from 'axios'
+import personService from './services/persons'
+import Notification from './components/Notification'
+
+const App = () => {
+  const [persons, setPersons] = useState([])
+  const [newName, setNewName] = useState('')
+  const [newNumber, setNewNumber] = useState('')
+  const [searchName, setSearchName] = useState('')
+  const [notificationMessage, setNotificationMessage] = useState(null)
+
+
+  useEffect(() => {
+    personService.getAll()
+      .then(response => {
+        setPersons(response)
+      })
+  }, [])
+
+  const addName = (event) => {
+    event.preventDefault()
+    if (persons.some(person => person.name === newName)) {
+      if(persons.some(person => person.name === newName && person.number !== newNumber)) {
+        if (window.confirm(`${newName} is already added to phonebook, replace the old number with a new one?`)) {
+          const personToUpdate = persons.find(person => person.name === newName)
+          const updatedPerson = { ...personToUpdate, number: newNumber }
+          personService.update(personToUpdate.id, updatedPerson)
+            .then(response => {
+              setPersons(persons.map(person => person.id !== personToUpdate.id ? person : response))
+              setNewName('')
+              setNewNumber('')
+              setNotificationMessage(`Updated ${newName}'s number`)
+              setTimeout(() => {
+                setNotificationMessage(null)
+              }, 5000) // Clear the notification after 5 seconds
+            })
+            .catch(error => {
+              console.error('Error updating person:', error)
+              setNotificationMessage(`Information of ${newName} has already been removed from server`)
+              setTimeout(() => {
+                setNotificationMessage(null)
+              }, 5000) // Clear the notification after 5 seconds
+            })
+        }
+      } else {
+        alert(`${newName} is already added to phonebook`)
+        setNewName('')
+        setNewNumber('')
+        return
+      }
+    }
+    const personObject = {
+      name: newName,
+      number: newNumber
+    }
+
+    // setPersons(persons.concat(personObject))
+    // setNewName('')
+    // setNewNumber('')
+
+    personService.create(personObject)
+       .then(response => {
+        setPersons(persons.concat(response))
+        setNewName('')
+        setNewNumber('')
+        setNotificationMessage(`Added ${newName}`)
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000) // Clear the notification after 5 seconds
+       })
+       .catch(error => {
+        console.error('Error adding person:', error)
+        setNotificationMessage(`${error.response.data.error}`)
+        setTimeout(() => {
+          setNotificationMessage(null)
+        }, 5000) // Clear the notification after 5 seconds
+
+       })
+  }
+
+  const handleSearch = (event) => {
+    setSearchName(event.target.value)
+  }
+
+  const handleDelete = (id) => {
+    console.log('Deleting person with id:', id)
+    if (window.confirm('Are you sure you want to delete this person?')) {
+      personService.deletePerson(id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== id))
+        })
+        .catch(error => {
+          console.error('Error deleting person:', error)
+          setNotificationMessage('Error deleting person')
+          setTimeout(() => {
+            setNotificationMessage(null)
+          }, 5000) // Clear the notification after 5 seconds
+        })
+    }
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <Notification message={notificationMessage} />
+      <Filter searchName={searchName} handleSearch={handleSearch} />
+      <h2>Add a new</h2>
+      <PersonForm addName={addName} newName={newName} setNewName={setNewName} newNumber={newNumber} setNewNumber={setNewNumber} />
+      <h2>Numbers</h2>
+      {searchName === '' ? (
+        persons.map(person => <Person key={person.id} name={person.name} number={person.number} onDelete={() => handleDelete(person.id)} />)
+      ) : (
+        persons.filter(person => person.name.toLowerCase().includes(searchName.toLowerCase()))
+          .map(person => <Person key={person.id} name={person.name} number={person.number} onDelete={() => handleDelete(person.id)}  />)
+      )}
+    </div>
+  )
+}
+
+const Filter = ({ searchName, handleSearch }) => {
+  return (
+    <div>
+      filter shown with: <input value={searchName} onChange={handleSearch}/>
+    </div>
+  )
+}
+
+const PersonForm = ({ addName, newName, setNewName, newNumber, setNewNumber }) => {
+  return (
+    <form>
+      <div>
+        name: <input value={newName} onChange={(event) => setNewName(event.target.value)  }/>
+        number: <input value={newNumber} onChange={(event) => setNewNumber(event.target.value)  }/>
+      </div>
+      <div>
+        <button type="submit" onClick= {addName}>add</button>
+      </div>
+    </form>
+  )
+}
+
+const Person = (props) => {
+  console.log(props)
+  return (
+    <div>
+      {props.name} {props.number}
+      <button onClick={props.onDelete}>delete</button>
+    </div>
+  )
+}
+
+export default App
--- Part_3/phonebook_backend/index.js
+++ Part_3/phonebook_backend/index.js
@@ -1,4 +1,6 @@
+const dotenv = require('dotenv').config()
 const { response } = require('express')
+const Person = require('./models/persons')
 const morgan = require('morgan')
 
 express = require('express')
@@ -12,74 +14,113 @@ morgan.token('body', (req) => {
 });
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      "id": "1",
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": "2",
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": "3",
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": "4",
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
+// let persons = [
+//     { 
+//       "id": "1",
+//       "name": "Arto Hellas", 
+//       "number": "040-123456"
+//     },
+//     { 
+//       "id": "2",
+//       "name": "Ada Lovelace", 
+//       "number": "39-44-5323523"
+//     },
+//     { 
+//       "id": "3",
+//       "name": "Dan Abramov", 
+//       "number": "12-43-234345"
+//     },
+//     { 
+//       "id": "4",
+//       "name": "Mary Poppendieck", 
+//       "number": "39-23-6423122"
+//     }
+// ]
 
 app. get('/info', (req, res) => {
-    let response = `Phonebook has info for ${persons.length} people <br> ${new Date()}`
-    res.send(response)
+    Person.find({}).then(persons => {
+        let response = `Phonebook has info for ${persons.length} people <br> ${new Date()}`
+        res.send(response)
+    }).catch(error => {
+        next(error)
+    })
 })
 
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(persons => {
+        res.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    const person = persons.find(p => p.id === id)
-    if (person) {
-        res.json(person)
-    } else {
-        res.status(404).end()
-    }
+    Person.findById(id).then(person => {
+        if (person) {
+            res.json(person)
+        } else {
+            res.status(404).end()
+        }
+    }).catch(error => {
+        next(error)
+    })
 })
 
 app.delete('/api/persons/:id', (req, res) => {
     const id = req.params.id
-    persons = persons.filter(p => p.id !== id)
-    res.status(204).end()
+    // persons = persons.filter(p => p.id !== id)
+    Person.findByIdAndDelete(id).then(
+        result => {res.status(204).end() }
+    )
 })
 
-app.post('/api/persons', (req, res) => {
+app.post('/api/persons', (req, res, next) => {
     const body = req.body
     if (!body.name || !body.number) {
         return res.status(400).json({ error: 'name or number is missing' })
     }
-    if (persons.find(p => p.name === body.name)) {
-        return res.status(400).json({ error: 'name must be unique' })
-    }
-    const person = {
-        id: (Math.random() * 1000000).toFixed(0),
+
+    const p = new Person({
         name: body.name,
         number: body.number
-    }
-    persons = persons.concat(person)
-    res.json(person)
+    })
+    p.save().then(savedPerson => {
+        res.json(savedPerson)
+    }).catch(error => {
+            next(error)
+        })
+}
+)
+
+app.put('/api/persons/:id', (req, res, next) => {
+    const id = req.params.id
+    const name = req.body.name
+    const number = req.body.number
+    Person.findById(id).then(updatedPerson => {
+        if (!updatedPerson) {
+            return res.status(404).json({ error: 'person not found' })
+        }
+        updatedPerson.name = name
+        updatedPerson.number = number
+        updatedPerson.save().then(updatedPerson => {
+            res.json(updatedPerson)
+        }).catch(error => {
+            next(error)
+        })
+    })
 })
 
-app.get('/', (req, res) => {
-    res.redirect('/index.html')
-})
+    app.get('/', (req, res) => {
+        res.redirect('/index.html')
+    })
+
+
+const errorHandler = (error, req, res, next) => {
+    console.error(error.message)
+    res.status(500).send({ error: error.message })
+    next(error)
+}
+
+app.use(errorHandler)
 
 PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
